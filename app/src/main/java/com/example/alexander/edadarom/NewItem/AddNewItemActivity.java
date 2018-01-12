409bdcaf64b087f7657be1f1598068aadb50b4a3
feanche <feanche@gmail.com>
2018-01-12 10:48:27 +0300

    getting photo

--- app/src/main/java/com/example/alexander/edadarom/NewItem/AddNewItemActivity.java
+++ app/src/main/java/com/example/alexander/edadarom/NewItem/AddNewItemActivity.java
@@ -1,13 +1,26 @@
 package com.example.alexander.edadarom.NewItem;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.alexander.edadarom.R;
@@ -16,8 +29,14 @@ import com.example.alexander.edadarom.models.UserModel;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 /**
  * Created by Alexander on 10.01.2018.
@@ -29,6 +48,11 @@ public class AddNewItemActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
     private ImageView backButton;
+    Uri file;
+    private ImageButton photoButton;
+    Target target;
+
+    final static String TAG = "myLogs_AddNewItem";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +62,11 @@ public class AddNewItemActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         button = (Button)findViewById(R.id.button2);
         backButton = (ImageView) findViewById(R.id.iv_close);
+        photoButton = (ImageButton) findViewById(R.id.imageButton);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -63,7 +92,85 @@ public class AddNewItemActivity extends AppCompatActivity {
         };
 
         backButton.setOnClickListener(backToMainActivity);
+
+        View.OnClickListener photoButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        };
+
+        photoButton.setOnClickListener(photoButtonListener);
     }
 
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = getOutputMediaFile();
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(intent, 100);
+    }
+
+
+    private Uri getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+        if(!mediaStorageDir.exists()){
+            if(!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File path = new File(mediaStorageDir.getPath()+File.separator+"IMG_"+timeStamp+".jpg");
+        Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName()+".com.example.alexander.edadarom.provider",path);
+        return photoUri;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode ==100) {
+            if (resultCode == RESULT_OK) {
+                upload(file);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void upload(Uri file) {
+        target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Log.d(TAG, "onBitmapLoaded");
+                uploadImage(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                Log.d(TAG,"onBitmapFailed");
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                Log.d(TAG,"onPrepareLoad");
+
+            }
+        };
+        Picasso.with(getApplicationContext()).load(file).resize(1000,1000).into(target);
+    }
+
+    public void uploadImage(Bitmap bitmap) {
+
+        photoButton.setImageBitmap(bitmap);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/alexander/edadarom/utils/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.example.alexander.edadarom.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Alexander on 12.01.2018.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
